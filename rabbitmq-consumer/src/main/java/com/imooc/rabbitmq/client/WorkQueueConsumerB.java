8ac7a5f17ae18074f4a49d161bd74dc0cd56b283
lius <edmondLau1018@foxmail.com>
2024-06-17 16:31:54 +0800

    rabbitMQ 工作队列模式

---
+++ rabbitmq-consumer/src/main/java/com/imooc/rabbitmq/client/WorkQueueConsumerB.java
@@ -0,0 +1,43 @@
+package com.imooc.rabbitmq.client;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class WorkQueueConsumerB {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //  新建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+
+        //  连接信息
+        factory.setHost("192.168.32.100");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        factory.setUsername("imooc");
+        factory.setPassword("imooc");
+
+        //  创建 connection 会话
+        Connection connection = factory.newConnection();
+
+        //  创建 channel 信道
+        Channel channel = connection.createChannel();
+        //  创建队列
+        AMQP.Queue.DeclareOk workQueue = channel.queueDeclare("work_queue", true, false, false, null);
+
+        //  创建消费者监听队列
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("work_queue_b body  =  " + new String(body));
+            }
+        };
+
+        //  信道 创建消费者 绑定队列和消费者的关系 监听队列消费消息
+        channel.basicConsume("work_queue", true, defaultConsumer);
+
+        //  不关闭资源 保持监听 ...
+
+    }
+}
---
+++ rabbitmq-producer/src/main/java/com/imooc/rabbitmq/client/WorkQueueProducer.java
@@ -0,0 +1,52 @@
+package com.imooc.rabbitmq.client;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class WorkQueueProducer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //  创建 rabbitMQ 客户端连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //  给连接工厂添加连接信息
+        connectionFactory.setHost("192.168.32.100");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername("imooc");
+        connectionFactory.setPassword("imooc");
+
+        //  创建连接会话 connection
+        Connection connection = connectionFactory.newConnection();
+        //  创建连接信道 channel
+        Channel channel = connection.createChannel();
+        //  新建 或者连接队列 （简单模式不使用交换机）
+        /**
+         * queue: 队列名称
+         * durable：是否持久化，true MQ 重启时队列不会消失 ，false  MQ 重启时队列会消失
+         * exclusive：是否只允许一个消费者监听这个队列 true：只允许一个消费者监听队列
+         * autoDelete：在当前队列没有消费者监听的时候是否自动删除
+         * arguments：创建当前队列需要的其他参数
+         */
+        AMQP.Queue.DeclareOk workQueue = channel.queueDeclare("work_queue", true, false, false, null);
+        //  通过信道 channel 直接发送消息
+        /**
+         * exchange：交换机名称 如果没有 填 ”“
+         * routingKey：路由规则，简单工作模式不需要使用 写 队列名称
+         * props：配置信息
+         * body: 消息数据
+         */
+        for (int i = 0; i < 30; i++) {
+            String messageBody = "work_queue test times + [ " + i + " ]";
+            channel.basicPublish("", "work_queue", null, messageBody.getBytes(StandardCharsets.UTF_8));
+        }
+        //  释放资源，关闭channel 信道和connection 连接
+        channel.close();
+        connection.close();
+    }
+}
