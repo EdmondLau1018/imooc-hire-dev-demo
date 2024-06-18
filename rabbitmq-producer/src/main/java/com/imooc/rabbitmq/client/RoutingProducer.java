2be39bd51e13bb347862aea55a7da177b1227c50
lius <edmondLau1018@foxmail.com>
2024-06-18 10:17:55 +0800

    rabbitMQ routing 路由模式

---
+++ rabbitmq-producer/src/main/java/com/imooc/rabbitmq/client/RoutingProducer.java
@@ -0,0 +1,80 @@
+package com.imooc.rabbitmq.client;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class RoutingProducer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //  创建 rabbitMQ 客户端连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //  给连接工厂添加连接信息
+        connectionFactory.setHost("192.168.32.100");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername("imooc");
+        connectionFactory.setPassword("imooc");
+
+        //  创建连接会话 connection
+        Connection connection = connectionFactory.newConnection();
+        //  创建连接信道 channel
+        Channel channel = connection.createChannel();
+
+        //  新建交换机 选择 DIRECT 广播类型
+        /**
+         * String exchange 交换机名称
+         * String type 枚举类 交换机类型
+         *     DIRECT("direct"),  根据 routing-key 匹配队列
+         *     FANOUT("fanout"),  所有绑定的队列都能收到消息
+         *     TOPIC("topic"),    符合 routing-pattern 的队列可以收到消息
+         *     HEADERS("headers"); 参数匹配 不常用
+         * boolean durable 是否持久化
+         * boolean autoDelete 是否自动删除
+         * boolean internal 是否是 rabbitMQ 内部使用的交换机 默认 false
+         *  Map<String, Object> arguments 其他参数
+         *
+         */
+        String routingExchange = "routing_exchange";
+        channel.exchangeDeclare(routingExchange, BuiltinExchangeType.DIRECT, true, false, false, null);
+
+        //  新建队列
+        String routingQueueOrder = "routing_queue_order";
+        channel.queueDeclare(routingQueueOrder, true, false, false, null);
+        String routingQueuePay = "routing_queue_pay";
+        channel.queueDeclare(routingQueuePay, true, false, false, null);
+
+        //  绑定 队列和交换机（通过信道 channel 进行绑定）
+        //  参数1：队列名称 参数2：交换机名称 参数3：routing-key
+        channel.queueBind(routingQueueOrder, routingExchange, "order_create");
+        channel.queueBind(routingQueueOrder, routingExchange, "order_update");
+        channel.queueBind(routingQueuePay, routingExchange, "order_pay");
+
+        //  通过信道 channel 直接发送消息
+        String msg1 = "创建订单A";
+        String msg2 = "创建订单B";
+        String msg3 = "修改订单C";
+        String msg4 = "修改订单D";
+        String msg5 = "支付订单E";
+        String msg6 = "支付订单F";
+
+        //  将不同消息 通过channel 不同的 routing-key 发送出去
+        //  exchange, routingKey, mandatory, immediate, props, body
+        channel.basicPublish(routingExchange, "order_create", null, msg1.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(routingExchange, "order_create", null, msg2.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(routingExchange, "order_update", null, msg3.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(routingExchange, "order_update", null, msg4.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(routingExchange, "order_pay", null, msg5.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(routingExchange, "order_pay", null, msg5.getBytes(StandardCharsets.UTF_8));
+
+
+        //  释放资源，关闭channel 信道和connection 连接
+        channel.close();
+        connection.close();
+    }
+}
