cebfb6b205e9768f918c386f3937cf51172cc010
lius <edmondLau1018@foxmail.com>
2024-06-28 13:23:05 +0800

    修改启动类注解

--- service-file-5001/src/main/java/com/imooc/FileApplication.java
+++ service-file-5001/src/main/java/com/imooc/FileApplication.java
@@ -4,6 +4,7 @@ import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 文件服务启动类
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
         DataSourceAutoConfiguration.class,
         SeataAutoConfiguration.class
 })
+@EnableDiscoveryClient
 public class FileApplication {
 
     public static void main(String[] args) {
--- service-resource-4001/src/main/java/com/imooc/ResourceApplication.java
+++ service-resource-4001/src/main/java/com/imooc/ResourceApplication.java
@@ -1,11 +1,12 @@
 package com.imooc;
 
+import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = SeataAutoConfiguration.class)
 public class ResourceApplication {
 
     public static void main(String[] args) {
