4c3f02be93f539434716e8228c72b0ed8cb49fe8
lius <edmondLau1018@foxmail.com>
2024-07-01 14:27:08 +0800

    work-service 包结构问题修复

--- service-work-3001/src/main/java/com/imooc/WorkServiceApplication.java
+++ service-work-3001/src/main/java/com/imooc/WorkServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan(basePackages = "com.com.imooc.mapper")
+@MapperScan(basePackages = "com.imooc.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class WorkServiceApplication {
