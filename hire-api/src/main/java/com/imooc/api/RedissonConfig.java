fc196b3eeddf10a2d4080ecaa8e45f98f5deb3fe
lius <edmondLau1018@foxmail.com>
2024-07-08 15:26:18 +0800

    redisson 客户端其他配置

--- hire-api/src/main/java/com/imooc/api/RedissonConfig.java
+++ hire-api/src/main/java/com/imooc/api/RedissonConfig.java
@@ -20,7 +20,13 @@ public class RedissonConfig {
         Config config = new Config();
         config.useSingleServer()
                 .setAddress("redis://192.168.32.100:6379")
-                .setPassword("1018");
+                .setPassword("1018")
+                .setDatabase(0)                         //  设置 redis 使用的数据库
+                .setConnectionMinimumIdleSize(10)       // 设置最小空闲连接数
+                .setConnectionPoolSize(20)              //  设置连接池的大小
+                .setIdleConnectionTimeout(60 * 1000)    //  销毁超时最大连接数，当连接数超过最小连接数且超时的时候就会被销毁
+                .setConnectTimeout(15 * 1000)           //  客户端 获得 redis 连接的响应超时时间
+                .setTimeout(15 * 1000);                  //  响应的超时时间
 
         return Redisson.create(config);
     }
