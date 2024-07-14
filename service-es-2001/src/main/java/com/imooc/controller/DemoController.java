b36a2ffd10020e6bf022e8f183223b2e2b701639
lius <edmondLau1018@foxmail.com>
2024-07-14 17:34:15 +0800

    保存数据

--- service-es-2001/src/main/java/com/imooc/controller/DemoController.java
+++ service-es-2001/src/main/java/com/imooc/controller/DemoController.java
@@ -3,6 +3,10 @@ package com.imooc.controller;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.pojo.eo.Stu;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +35,25 @@ public class DemoController {
     @PostMapping("/deleteIndex")
     public GraceJSONResult deleteIndex() {
         esTemplate.indexOps(Stu.class).delete();
+        return GraceJSONResult.ok();
+    }
+
+    @PostMapping("/save")
+    public GraceJSONResult saveIndex() {
+        Stu stu = new Stu();
+        stu.setStuId(1001L);
+        stu.setName("imooc");
+        stu.setAge(21);
+        stu.setDescription("慕课网的学生");
+        stu.setMoney(100.2f);
+
+        //  构建 indexQuery 对象
+        IndexQuery iq = new IndexQueryBuilder().withObject(stu).build();
+
+        //  创建 indexCoordinate 对象
+        IndexCoordinates ic = IndexCoordinates.of("stu");
+
+        esTemplate.index(iq, ic);
         return GraceJSONResult.ok();
     }
 }
