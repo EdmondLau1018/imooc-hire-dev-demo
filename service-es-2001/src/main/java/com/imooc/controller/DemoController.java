eed587db8124aa09db3454329347fbb0b81616fb
lius <edmondLau1018@foxmail.com>
2024-07-15 15:24:44 +0800

    ES 数据分页全量查询 （构建查询条件）

--- service-es-2001/src/main/java/com/imooc/controller/DemoController.java
+++ service-es-2001/src/main/java/com/imooc/controller/DemoController.java
@@ -3,14 +3,19 @@ package com.imooc.controller;
 import com.imooc.grace.result.GraceJSONResult;
 import com.imooc.pojo.eo.SearchResumesEO;
 import com.imooc.pojo.eo.Stu;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/es")
@@ -56,5 +61,35 @@ public class DemoController {
 
         esTemplate.index(iq, ic);
         return GraceJSONResult.ok();
+    }
+
+    /**
+     * 分页从 ES 全量查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @PostMapping("/matchAll")
+    public GraceJSONResult matchAll(Integer page, Integer pageSize) {
+
+        if (page < 1) page = 1;
+        //ES 的分页是从 0 开始的
+        page--;
+
+        PageRequest pageable = PageRequest.of(page, pageSize);
+        //  多条件查询构造器
+        Query query = new NativeSearchQueryBuilder().withQuery(QueryBuilders.matchAllQuery())
+                .withPageable(pageable)
+                .build();
+
+        SearchHits<SearchResumesEO> hits = esTemplate.search(query, SearchResumesEO.class);
+        List<SearchHit<SearchResumesEO>> hitList = hits.getSearchHits();
+        // 从 ES 查询结果 hits 中获取 返回结果 list
+        ArrayList<SearchResumesEO> list = new ArrayList<>();
+        for (SearchHit<SearchResumesEO> searchHit : hitList) {
+            SearchResumesEO content = searchHit.getContent();
+            list.add(content);
+        }
+        return GraceJSONResult.ok(list);
     }
 }
